36f73e8306687bfda20b536432e57bb47a16492a
IamSboby <rodymichael150110@gmail.com>
2026-09-22 20:42:55 +0200

    Primo modulo esercizi

---
+++ 01 - IO ed elaborazione dati basici/Es5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EserciziVari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double spigolo, volume, faccia, facceTot;
+
+            Console.Write("Inserire primo numero: ");
+            spigolo = Convert.ToDouble(Console.ReadLine());
+
+            volume = Math.Pow(spigolo,3);
+            faccia = Math.Pow(spigolo, 2);
+            facceTot = faccia * 6;
+
+            Console.Write("\nIl volume equivale a: " + volume);
+            Console.Write("\nIl volume equivale a: " + faccia);
+            Console.Write("\nIl volume equivale a: " + facceTot);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 - IO ed elaborazione dati basici/Es8.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Inserisci i litri di olio da acquistare: ");
+        double litriPartitaOlio = Convert.ToDouble(Console.ReadLine());
+        const int prezzoOlioNormale = 15, prezzoOlioPremium = 20;
+        double prezzoSpesa =(double) ((litriPartitaOlio*prezzoOlioNormale)+(litriPartitaOlio * prezzoOlioPremium));
+        Console.WriteLine("Prezzo della spesa:" + prezzoSpesa);
+    }
+}
+
+//il programma credo non usi certe librerie come su visual studio dato che io uso gia da anni VS Code e ho usato direttamente quello
