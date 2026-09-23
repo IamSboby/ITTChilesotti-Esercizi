e840d7a60b01d9cdc0efe21ab942900d80e0222d
Sboby <ralfonso@chilesotti.it>
2026-09-23 08:57:06 +0200

    Nono Esercizio: Calcolo di percentuali e arrotondamento (Math)
    È stato aggiunto un Console.ReadKey() per mettere in pausa l'output finché non viene premuto un tasto.

--- 01 - IO ed elaborazione dati basici/Es9.cs
+++ 01 - IO ed elaborazione dati basici/Es9.cs
@@ -22,6 +22,8 @@ class Program
         Console.WriteLine("Percentuale di votanti: " + percentualeVotanti);
         Console.WriteLine("Percentuale di voti favorevoli: " + Math.Round(percentualeSi, 2) + "%"); //* (arrotondato a 2 cifre decimali)
         Console.WriteLine("Percentuale di voti non favorevoli: " + Math.Round(100 - percentualeSi, 2) + "%"); //* (anche qua arrotondato a 2 cifre decimali)
+
+        Console.ReadKey()
     }
 }
 
