dfd6ece9135177cde647e07f19be50e68ff55769
IamSboby <rodymichael150110@gmail.com>
2026-09-24 19:39:43 +0200

    Primo Esericzio: output stringa

--- 01 - IO ed elaborazione dati basici/Es1.cs
+++ 01 - IO ed elaborazione dati basici/Es1.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rody
+namespace StampaNome //Nome del progetto
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Rody");
-            Console.Read();
+            Console.WriteLine("IlTuoNome"); //Console.WriteLine stampa il contenuto tra virgolette nella console
+
+            Console.Read(); //aspetta che l'utente prema un tasto prima di chiudere la console
         }
     }
 }
