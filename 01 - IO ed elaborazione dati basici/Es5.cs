f735986aab5e409b465751b24b24eed1ca04d48f
IamSboby <rodymichael150110@gmail.com>
2026-09-24 20:20:18 +0200

    Quinto Esercizio: calcolo del cubo (uso della classe math)

--- 01 - IO ed elaborazione dati basici/Es5.cs
+++ 01 - IO ed elaborazione dati basici/Es5.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EserciziVari
+namespace Esercizio5 //Nome del progetto
 {
     class Program
     {
@@ -15,8 +15,8 @@ namespace EserciziVari
             Console.Write("Inserire primo numero: ");
             spigolo = Convert.ToDouble(Console.ReadLine());
 
-            volume = Math.Pow(spigolo,3);
-            faccia = Math.Pow(spigolo, 2);
+            volume = Math.Pow(spigolo,3); //usando la "classe"(una funzione con più funzioni dentro) Math possiamo fare operazioni matematiche più complesse come la potenza, la radice quadrata ecc. senza dover scrivere il codice per calcolare queste operazioni da zero
+            faccia = Math.Pow(spigolo, 2); //usando la "sottofunzione" (pow) possiamo calcolare la potenza di un numero, usando questo formato: math.Pow(numero, potenza)
             facceTot = faccia * 6;
 
             Console.Write("\nIl volume equivale a: " + volume);
