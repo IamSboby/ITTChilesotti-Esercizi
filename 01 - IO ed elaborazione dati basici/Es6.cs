ae5eaa828d2a7c5b3f7d2fbb348730ba49daf17f
IamSboby <rodymichael150110@gmail.com>
2026-09-24 20:24:16 +0200

    Sesto Esercizio: "swap" dei valori di una variabile

--- 01 - IO ed elaborazione dati basici/Es6.cs
+++ 01 - IO ed elaborazione dati basici/Es6.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EserciziVari
+namespace Esercizio6 //Nome del progetto
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
             double VarA, VarB, TempVar;
@@ -19,9 +18,13 @@ namespace EserciziVari
             Console.Write("Inserire primo numero: ");
             VarB = Convert.ToDouble(Console.ReadLine());
 
-            TempVar = VarB;
-            VarB = VarA;
-            VarA = TempVar;
+            //per fare lo "swap" delle variabili, dobbiamo creare una variabile temporanea che ci permetta di salvare il valore di una delle due variabili, altrimenti quando andremo a fare l'assegnazione della prima variabile alla seconda, perderemo il valore della seconda variabile e quindi non potremo più assegnarlo alla prima variabile
+
+            TempVar = VarB;     //salviamo il valore della variabile B nella variabile temporanea
+            VarB = VarA;        //assegniamo il valore della variabile A alla variabile B
+            VarA = TempVar;     //assegniamo il valore della variabile temporanea (che contiene il valore della variabile B) alla variabile A
+
+            Console.WriteLine("Dopo lo swap:\nVarA = " + VarA + "\nVarB = " + VarB);
 
             Console.ReadKey();
         }
