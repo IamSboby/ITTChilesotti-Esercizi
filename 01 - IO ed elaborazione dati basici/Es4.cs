5b95c1c0f3780efcc344f74a8ba10f542992d7ab
IamSboby <rodymichael150110@gmail.com>
2026-09-24 19:53:57 +0200

    Quarto Esercizio: calcolo area rettangolo

--- 01 - IO ed elaborazione dati basici/Es4.cs
+++ 01 - IO ed elaborazione dati basici/Es4.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EserciziVari
+namespace Esercizio4 //Nome del progetto
 {
     class Program
     {
@@ -12,15 +12,17 @@ namespace EserciziVari
         {
             double Base, Altezza;
 
-            Console.Write("Inserire Base: ");
-            Base = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Inserire Base: "); 
+            Base = Convert.ToDouble(Console.ReadLine()); 
 
             Console.Write("\nInserire Altezzza: ");
             Altezza = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("\nIl perimetro è uguale a: " + ((Base*2)+(Altezza*2)));
-            Console.Write("\nL' area è uguale a: " + (Base*Altezza));
+            Console.Write("\nIl perimetro è uguale a: " + ((Base*2)+(Altezza*2))); //possiamo anche fare operazioni matematiche direttamente dentro il Console.Write() senza dover creare una variabile apposita
+            Console.Write("\nL' area è uguale a: " + (Base*Altezza)); //IMPORTANTE: ricordarsi di mettere le parentesi tonde intorno alle operazioni matematiche, altrimenti il programma non saprà come calcolare l'operazione e darà errore o la calcolera sbagliata
+            
             Console.ReadKey();
+            //basta nn ho più sbatti di scrivere i commenti mi sto gia rompendo
         }
     }
 }
