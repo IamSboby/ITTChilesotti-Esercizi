481da3d99df438b331badd9b1b42a2a6f2166e50
IamSboby <rodymichael150110@gmail.com>
2026-09-25 18:55:33 +0200

    Undicesimo Esercizio: Uso dell' operatore "modulo" (%)

--- 01 - IO ed elaborazione dati basici/Es11.cs
+++ 01 - IO ed elaborazione dati basici/Es11.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NomeProgetto //Nome del progetto
+namespace Esercizio11 //Nome del progetto
 {
     class Program
     {
