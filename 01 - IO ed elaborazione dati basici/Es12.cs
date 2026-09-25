5556f2a91b0b066d96441d71b9a51eda5f558c0d
IamSboby <rodymichael150110@gmail.com>
2026-09-25 19:13:56 +0200

    Dodicesimo Esercizio: L' "else if" e le condizioni incatenate

--- 01 - IO ed elaborazione dati basici/Es12.cs
+++ 01 - IO ed elaborazione dati basici/Es12.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Esercizio12 //Nome del progetto
+namespace Esercizio12 //Nome del progetto 
 {
     class Program
     {
