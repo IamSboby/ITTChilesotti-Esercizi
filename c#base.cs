78f0120594e969584d2849d48c6be16631ee1b92
IamSboby <rodymichael150110@gmail.com>
2026-09-25 17:44:51 +0200

    base per fare un c# come lo da Visual Studio

---
+++ c#base.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomeProgetto //Nome del progetto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inserire qua il codice da eseguire
+        }
+    }
+}
